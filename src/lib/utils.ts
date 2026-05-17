55b6d123b60cdee5832c86b3452a7f4a7172b009
Skip <skip@usecurling.com>
2026-05-17 09:46:44 +0000

    Auto-commit after full sync (build simulation)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,11 +2,11 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-/**
- * Merges multiple class names into a single string
- * @param inputs - Array of class names
- * @returns Merged class names
- */
+-- /**
+--  * Merges multiple class names into a single string
+-- * @param inputs - Array of class names
+-- * @returns Merged class names
+-- */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
